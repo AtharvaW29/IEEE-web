540892eef6e9b45b298d1927e0217be740b6f316
Atharva Wagh <93931071+AtharvaW29@users.noreply.github.com>
2022-08-15 00:40:33 +0530

    Add files via upload

---
+++ main-page/assets/Js/index.js
@@ -0,0 +1,43 @@
+var $cont = document.querySelector('.cont');
+var $elsArr = [].slice.call(document.querySelectorAll('.el'));
+var $closeBtnsArr = [].slice.call(document.querySelectorAll('.el__close-btn'));
+
+setTimeout(function() {
+  $cont.classList.remove('s--inactive');
+}, 200);
+
+$elsArr.forEach(function($el) {
+  $el.addEventListener('click', function() {
+    if (this.classList.contains('s--active')) return;
+    $cont.classList.add('s--el-active');
+    this.classList.add('s--active');
+  });
+});
+
+$closeBtnsArr.forEach(function($btn) {
+  $btn.addEventListener('click', function(e) {
+    e.stopPropagation();
+    $cont.classList.remove('s--el-active');
+    document.querySelector('.el.s--active').classList.remove('s--active');
+  });
+});
+
+//Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }
+    else{
+        nav.classList.remove("header-scrolled");
+    }
+}
+
+// Menu Hide
+let navBar= document.querySelectorAll(".nav-link");
+let navCollapse = document.querySelector(".navbar-collapse.collapse");
+navBar.forEach(function(a){
+    a.addEventListener("click", function(){
+        navCollapse.classList.remove("show");
+    })
+})
